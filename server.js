c25b4af695902b4fd232af1b565c79ea835e5e79
chhavitekriwal <chhavi.tekriwal@gmail.com>
2022-08-01 17:02:06 +0530

    add frontend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { urlencoded } = require("body-parser");
 const apiKey = process.env.API_KEY;
 const PORT = process.env.PORT || 3000;
 app.use(parser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
@@ -19,8 +20,9 @@ app.post("/",(req,res)=>{
             const desc = weatherData.weather[0].description;
             const temp = weatherData.main.temp;
             const icon = "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png";
-            res.write("<h1>The temperature in "+weatherData.name+" is "+temp+" degrees Celsius and the weather is "+desc+".</h1>");
+            res.write('<head><link rel="stylesheet" type="text/css" href="style.css" /></head>')
             res.write("<img src="+icon+">");
+            res.write("<h1>The temperature in "+weatherData.name+" is "+temp+" degrees Celsius and the weather is "+desc+".</h1>");
             res.send();
         })
     })
